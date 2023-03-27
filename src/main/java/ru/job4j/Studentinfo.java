c1fd4695137bf93262f1daa629e534d2b4d33cfd
KachurAndrey <ntx03@yandex.ru>
2023-03-27 16:57:36 +0500

    Добавил класс Multiple

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String args[]){
+        System.out.println("Kachur Andrey");
+        System.out.println("27.03.2023");
+    }
+}
